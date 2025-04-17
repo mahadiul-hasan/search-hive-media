d088bc78d966dca3eea7a23d5a48d1c984b1fa27
mahadiul-hasan <mahadiulhasan49@gmail.com>
2025-04-18 05:06:42 +0600

    updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,12 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-	cors({
-		origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
-		credentials: true,
-	})
-);
+app.use(cors());
 
 app.use("/", redirectRouter);
 app.use("/v1/", routers);
